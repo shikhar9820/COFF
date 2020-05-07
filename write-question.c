3397ee84d5cb14c8a8646e5deb8ac816ff25585d
shikhar9820 <shikhar.kataruka@gmail.com>
2020-05-07 19:29:07 +0530

    File handling code for write-question is created

--- write-question.c
+++ write-question.c
@@ -55,6 +55,31 @@
                     printf("Output\n");
                     scanf("%s",q.test[i].output);
         }
+        FILE *outfile; 
+      
+    // open file for writing 
+    outfile = fopen ("question.txt", "w"); 
+    if (outfile == NULL) 
+    { 
+        fprintf(stderr, "\nError opend file\n"); 
+        exit (1); 
+    } 
+  
+    //struct person input1 = {1, "rohan", "sharma"}; 
+    //struct person input2 = {2, "mahendra", "dhoni"}; 
+      
+    // write struct to file 
+    fwrite (&q, sizeof(struct question), 1, outfile); 
+    //fwrite (&input2, sizeof(struct person), 1, outfile); 
+      
+    if(fwrite != 0)  
+        printf("contents to file written successfully !\n"); 
+    else 
+        printf("error writing file !\n"); 
+  
+    // close file 
+    fclose (outfile); 
+        return 0;
 } 
 
 
